3113c38aca6e496587adde438bc3fc72791792db
Steven Sherk <steven.sherk@se-eng.com>
2013-09-04 14:26:50 -0600

    leon: enable the SD card's USB port on Leon
    This is part of the initial mainboard changes for bring up
    It was discovered the SD card USB port wasn't enabled
    correctly.
    
    BUG=chrome-os-partner:22104
    BRANCH=leon
    TEST=manual: emerge-leon chromeos-coreboot-leon, SD card
    insertion
    
    Change-Id: Ic6ab2986542fd81c75746fbae3fc27709dbc226d
    Signed-off-by: Steven Sherk <steven.sherk@se-eng.com>
    Reviewed-on: https://chromium-review.googlesource.com/168100
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>
    Reviewed-by: Steve Goodrich <steve.goodrich@se-eng.com>
    Commit-Queue: Steve Goodrich <steve.goodrich@se-eng.com>
    Tested-by: Steve Goodrich <steve.goodrich@se-eng.com>

--- src/mainboard/google/leon/romstage.c
+++ src/mainboard/google/leon/romstage.c
@@ -171,9 +171,9 @@ void mainboard_romstage_entry(unsigned long bist)
 			  USB_PORT_MINI_PCIE },
 			{ 0x0040, 1, 2,               /* P4: Port B, CN6  */
 			  USB_PORT_BACK_PANEL },
-			{ 0x0000, 0, USB_OC_PIN_SKIP, /* P5: EMPTY */
-			  USB_PORT_SKIP },
-			{ 0x0150, 1, USB_OC_PIN_SKIP, /* P6: SD Card */
+			{ 0x0040, 1, USB_OC_PIN_SKIP, /* P5: SD Card */
+			  USB_PORT_INTERNAL },
+			{ 0x0150, 1, USB_OC_PIN_SKIP, /* P6: SIM CARD */
 			  USB_PORT_FLEX },
 			{ 0x0000, 0, USB_OC_PIN_SKIP, /* P7: EMPTY */
 			  USB_PORT_SKIP },
