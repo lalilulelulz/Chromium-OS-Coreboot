7020f8962c7cbcd88871ae54e1900a64cf586d84
Shawn Nematbakhsh <shawnn@chromium.org>
2013-11-12 11:58:28 -0800

    monroe: Initial mainboard commit
    Add files necessary for building for monroe board. Cloned entirely from
    Beltino with only string changes.
    
    BUG=chrome-os-partner:24020
    TEST=emerge-monroe chromeos-coreboot-monroe
    BRANCH=monroe
    
    Change-Id: Ic9d553759d6377e4de29955cf3416e9a529dca38
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/176546

---
+++ src/mainboard/google/monroe/onboard.h
@@ -0,0 +1,9 @@
+void lan_init(void);
+
+/* defines for programming the MAC address */
+#define MONROE_NIC_VENDOR_ID		0x10EC
+#define MONROE_NIC_DEVICE_ID		0x8168
+
+/* 0x00: White LINK LED and Amber ACTIVE LED */
+#define MONROE_NIC_LED_MODE		0x00
+
