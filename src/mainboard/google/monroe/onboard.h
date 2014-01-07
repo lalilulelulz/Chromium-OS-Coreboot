0a085d78ab9e3ea87e6e0a5f43a054cfddb7380c
Stefan Reinauer <reinauer@chromium.org>
2014-01-07 09:53:06 -0800

    monroe: Add ACPI code to support wake-on-lan
    There needs to be an ACPI linkage to provide the power resource
    needed to wake this device so the kernel will enable the SCI
    before going to suspend.
    
    A link is added for both NIC and WLAN, but it is only tested
    on the NIC.
    
    This is a forward port from Duncan's beltino patch.
    
    BUG=chrome-os-partner:24657
    BRANCH=monroe
    TEST=build and boot on monroe, suspend and wake with etherwake
    
    Change-Id: If16631ad28c5f4300adb1a1b134ab0a45f7ac162
    Original-Change-Id: I2804d2e904e26d6e34f5a177f0dabc1aaa3f0288
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/191104
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- src/mainboard/google/monroe/onboard.h
+++ src/mainboard/google/monroe/onboard.h
@@ -1,4 +1,9 @@
+#ifndef __MAINBOARD_ONBOARD_H
+#define __MAINBOARD_ONBOARD_H
+
+#ifndef __ACPI__
 void lan_init(void);
+#endif
 
 /* defines for programming the MAC address */
 #define MONROE_NIC_VENDOR_ID		0x10EC
@@ -7,3 +12,10 @@ void lan_init(void);
 /* 0x00: White LINK LED and Amber ACTIVE LED */
 #define MONROE_NIC_LED_MODE		0x00
 
+/* NIC wake is GPIO 8 */
+#define MONROE_NIC_WAKE_GPIO		8
+
+/* WLAN wake is GPIO 10 */
+#define MONROE_WLAN_WAKE_GPIO		10
+
+#endif
