039d9c8d2cefba2b2d8e890b56353a4ce5587329
Gediminas Ramanauskas <gedis@google.com>
2014-01-11 19:44:50 -0800

    zako: Initial mainboard commit
    Add files necessary for building for zako board. Cloned entirely from
    Beltino with only string changes.
    
    BUG=chrome-os-partner:24660
    TEST=emerge-zako chromeos-coreboot-zako
    BRANCH=zako
    
    Change-Id: I6129870edc5b041fcfbbce5994c8beef73cb6573
    Reviewed-on: https://chromium-review.googlesource.com/182238
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>
    Tested-by: Gediminas Ramanauskas <gedis@chromium.org>

---
+++ src/mainboard/google/zako/onboard.h
@@ -0,0 +1,9 @@
+void lan_init(void);
+
+/* defines for programming the MAC address */
+#define ZAKO_NIC_VENDOR_ID		0x10EC
+#define ZAKO_NIC_DEVICE_ID		0x8168
+
+/* 0x00: White LINK LED and Amber ACTIVE LED */
+#define ZAKO_NIC_LED_MODE		0x00
+
