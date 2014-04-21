91367317c6fdf2a9be6f8fc5432fd29091fe795a
Gediminas Ramanauskas <gedis@google.com>
2014-04-21 11:23:15 -0700

    Zako: updating thermal control table to v06
    BUG=none
    TEST=none
    
    Change-Id: I168c73984b27699c4f347cb6389fa628caa74d7d
    Reviewed-on: https://chromium-review.googlesource.com/195832
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>
    Tested-by: Gediminas Ramanauskas <gedis@chromium.org>

--- src/mainboard/google/zako/thermal.h
+++ src/mainboard/google/zako/thermal.h
@@ -23,27 +23,27 @@
 /* Fan is OFF */
 #define FAN4_THRESHOLD_OFF	0
 #define FAN4_THRESHOLD_ON	0
-#define FAN4_PWM		0x6b
+#define FAN4_PWM		0x4c
 
 /* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	40
-#define FAN3_THRESHOLD_ON	49
-#define FAN3_PWM		0x7c
+#define FAN3_THRESHOLD_OFF	48
+#define FAN3_THRESHOLD_ON	52
+#define FAN3_PWM		0x6d
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	45
+#define FAN2_THRESHOLD_OFF	50
 #define FAN2_THRESHOLD_ON	55
-#define FAN2_PWM		0x8c
+#define FAN2_PWM		0x7c
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	50
+#define FAN1_THRESHOLD_OFF	52
 #define FAN1_THRESHOLD_ON	58
-#define FAN1_PWM		0xbc
+#define FAN1_PWM		0xa3
 
 /* Fan is at FULL speed */
 #define FAN0_THRESHOLD_OFF	55
 #define FAN0_THRESHOLD_ON	60
-#define FAN0_PWM		0xed
+#define FAN0_PWM		0xba
 
 /* Temperature which OS will shutdown at */
 #define CRITICAL_TEMPERATURE	98
