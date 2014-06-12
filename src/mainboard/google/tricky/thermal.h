a15d02c16c948a035617c813a54558b47bdf647c
Justin Tsai <Justin_Tsai@quantatw.com>
2014-06-12 17:29:00 +0800

    CHERRY-PICK: google/tricky:  new thermal values
    Based on latest thermal report
    
    This value was recommended by thermal team of quanta
    "define PASSIVE_TEMPERATURE 95"
    
    BUG=chrome-os-partner:24532
    TEST=boot tested on tricky
    BRANCH=tricky
    
    Change-Id: If50512f831f4ab96c5dfe00eff477fe32f368515
    Original-Change-Id: I4b8639f926fc3cf57eb5329818b9b912bfbe222d
    Signed-off-by: Justin Tsai <Justin_Tsai@quantatw.com>
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-by: Mohammed Habibulla <moch@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/203585

--- src/mainboard/google/tricky/thermal.h
+++ src/mainboard/google/tricky/thermal.h
@@ -26,30 +26,30 @@
 #define FAN4_PWM		0x00
 
 /* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	48
-#define FAN3_THRESHOLD_ON	55
-#define FAN3_PWM		0x40
+#define FAN3_THRESHOLD_OFF	40
+#define FAN3_THRESHOLD_ON	50
+#define FAN3_PWM		0x5a
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	52
-#define FAN2_THRESHOLD_ON	64
-#define FAN2_PWM		0x80
+#define FAN2_THRESHOLD_OFF	58
+#define FAN2_THRESHOLD_ON	67
+#define FAN2_PWM		0xa6
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	60
-#define FAN1_THRESHOLD_ON	68
-#define FAN1_PWM		0xb0
+#define FAN1_THRESHOLD_OFF	67
+#define FAN1_THRESHOLD_ON	75
+#define FAN1_PWM		0xc0
 
 /* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	66
-#define FAN0_THRESHOLD_ON	78
+#define FAN0_THRESHOLD_OFF	85
+#define FAN0_THRESHOLD_ON	90
 #define FAN0_PWM		0xff
 
 /* Temperature which OS will shutdown at */
 #define CRITICAL_TEMPERATURE	100
 
 /* Temperature which OS will throttle CPU */
-#define PASSIVE_TEMPERATURE	90
+#define PASSIVE_TEMPERATURE	95
 
 /* Tj_max value for calculating PECI CPU temperature */
 #define MAX_TEMPERATURE		100
