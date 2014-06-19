d92045850963f4e628333028e84fae1282cf8050
Justin Tsai <Justin_Tsai@quantatw.com>
2014-06-19 14:16:42 +0800

    google/tricky:  new Fan/temperature table values
    BUG=chrome-os-partner:29773
    TEST=boot tested on tricky
    BRANCH=tricky
    
    Change-Id: I029ae885d9d84956cb7e8164a00335e82eb47bae
    Signed-off-by: Justin Tsai <Justin_Tsai@quantatw.com>
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-by: Mohammed Habibulla <moch@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/204447

--- src/mainboard/google/tricky/thermal.h
+++ src/mainboard/google/tricky/thermal.h
@@ -26,27 +26,27 @@
 #define FAN4_PWM		0x00
 
 /* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	40
-#define FAN3_THRESHOLD_ON	50
-#define FAN3_PWM		0x5a
+#define FAN3_THRESHOLD_OFF	50
+#define FAN3_THRESHOLD_ON	55
+#define FAN3_PWM		0x76
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	58
-#define FAN2_THRESHOLD_ON	67
-#define FAN2_PWM		0xa6
+#define FAN2_THRESHOLD_OFF	59
+#define FAN2_THRESHOLD_ON	65
+#define FAN2_PWM		0x98
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	67
+#define FAN1_THRESHOLD_OFF	68
 #define FAN1_THRESHOLD_ON	75
-#define FAN1_PWM		0xc0
+#define FAN1_PWM		0xbf
 
 /* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	85
-#define FAN0_THRESHOLD_ON	90
-#define FAN0_PWM		0xff
+#define FAN0_THRESHOLD_OFF	80
+#define FAN0_THRESHOLD_ON	86
+#define FAN0_PWM		0xdc
 
 /* Temperature which OS will shutdown at */
-#define CRITICAL_TEMPERATURE	100
+#define CRITICAL_TEMPERATURE	98
 
 /* Temperature which OS will throttle CPU */
 #define PASSIVE_TEMPERATURE	95
