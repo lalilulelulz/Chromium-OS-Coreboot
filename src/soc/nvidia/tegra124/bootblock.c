0916896dceaf703aafd6191052f177792d573bd6
Jimmy Zhang <jimmzhang@nvidia.com>
2014-07-02 17:45:18 -0700

    tegra124: Correct cpu power on sequence and CPUPWRGOOD_TIME
    Based on TRM, cpu clock enabling and reset vector setting should
    all be done properly before ungating cpu power partition. Otherwise,
    with current code, a race condition could occur where cpu starts but
    reset vector has not been set.
    
    BUG=chrome-os-partner:30064
    BRANCH=none
    TEST=run nyan_big reboot test. No issue is experienced.
    
    Signed-off-by: Jimmy Zhang <jimmzhang@nvidia.com>
    
    Change-Id: I571e128693bb2763ee673bd183b8cf60921dc475
    Reviewed-on: https://chromium-review.googlesource.com/206682
    Tested-by: Jimmy Zhang <jimmzhang@nvidia.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Jimmy Zhang <jimmzhang@nvidia.com>
    (cherry picked from commit 106480ff32406c899a24544fdfab858db5afd1d9)
    Reviewed-on: https://chromium-review.googlesource.com/208122
    Reviewed-by: Yen Lin <yelin@nvidia.com>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>
    Tested-by: Julius Werner <jwerner@chromium.org>

--- src/soc/nvidia/tegra124/bootblock.c
+++ src/soc/nvidia/tegra124/bootblock.c
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <assert.h>
 #include <arch/exception.h>
 #include <arch/hlt.h>
 #include <bootblock_common.h>
@@ -70,13 +71,13 @@ void main(void)
 			  PINMUX_PWR_INT_N_FUNC_PMICINTR |
 			  PINMUX_INPUT_ENABLE);
 
-	power_enable_cpu_rail();
-	power_ungate_cpu();
-
 	entry = cbfs_load_stage(CBFS_DEFAULT_MEDIA, "fallback/romstage");
 
-	if (entry)
-		clock_cpu0_config_and_reset(entry);
+	ASSERT(entry);
+	clock_cpu0_config(entry);
+
+	power_enable_and_ungate_cpu();
+	clock_cpu0_remove_reset();
 
 	clock_halt_avp();
 }
