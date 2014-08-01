56ff25e7b83b6f442e84d0d7f1642bd6a01ba261
Keith Tzeng <keith.tzeng@quantatw.com>
2014-08-01 17:36:55 +0800

    enguarde: Add support for new Samsung RAM K4B2G1646Q-BYK0
    Add support for K4B2G1646Q-BYK0 for dual channel config,
    and removed Hynix H5TC4G63AFR-PBA single channel config, which is never
    been used.
    
    BUG=chrome-os-partner:31096
    TEST=compile and power on success.
    
    Change-Id: I59018e18f1ecdf5a2a79dadb90b69a965340e7be
    Signed-off-by: Keith Tzeng <Keith.Tzeng@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/210639
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- src/mainboard/google/enguarde/romstage.c
+++ src/mainboard/google/enguarde/romstage.c
@@ -32,12 +32,12 @@
  * 0b010 - 2GiB total - 2 x 1GiB Micron MT41K128M16JT-125:K 1600MHz
  * 0b011 - 2GiB total - 2 x 1GiB Hynix  H5TC2G63FFR-PBA 1600MHz
  * 0b100 - 2GiB total - 1 x 2GiB Micron MT41K256M16HA-125:E 1600MHz
- * 0b101 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63AFR-PBA 1600MHz
+ * 0b101 - 2GiB total - 2 x 1GiB Samsung K4B2G1646Q-BYK0 1600MHz
  * 0b110 - 4GiB total - 2 x 2GiB Samsung K4B4G1646Q-HYK0 1600MHz
  * 0b111 - 2GiB total - 1 x 2GiB Samsung K4B4G1646Q-HYK0 1600MHz
  */
 static const uint32_t dual_channel_config =
-	(1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 6);
+	(1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 5) | (1 << 6);
 
 #define SPD_SIZE 256
 #define GPIO_SSUS_37_PAD 57
