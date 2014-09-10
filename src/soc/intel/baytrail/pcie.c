lalilulelulz/Chromium-OS-Coreboot

commit dd53d7f2de50a772c9a1ca43cab7d8431806bf2d

Baytrail: PCIe devices doesn't work if PCIe port 1 has no device.
Keep PCIe port 1 enabled even if it has no device.

BUG=chrome-os-partner:31768
TEST=PCIe devices on port 2/3/4 can work well if port 1 has no device.
BRANCH=NONE
Signed-off-by: Ted Kuo <tedkuo@ami.com.tw>

Change-Id: I56111173d54181cd29caf4a42d01ffd19e1bcb2a
Reviewed-on: https://chromium-review.googlesource.com/216979
Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
Reviewed-by: Ted Kuo <tedkuo@ami.com.tw>
Commit-Queue: Ted Kuo <tedkuo@ami.com.tw>
Tested-by: Ted Kuo <tedkuo@ami.com.tw>

src/soc/intel/baytrail/pcie.c
@@ -160,8 +160,11 @@ static void check_device_present(device_t dev)
 	/* Set slot implemented. */
 	pci_write_config32(dev, XCAP, pci_read_config32(dev, XCAP) | SI);
 
-	/* No device present. */
-	if (!(pci_read_config32(dev, SLCTL_SLSTS) & PDS)) {
+	/* All PCIe ports disappear if port1 is disabled.
+	 * Keep PCIe port1 enabled even if no PCIe device present. */
+	if (!(pci_read_config32(dev, SLCTL_SLSTS) & PDS) &&
+			(is_first_port() == false)) {
+		/* No device present. */
 		printk(BIOS_DEBUG, "No PCIe device present.\n");
 		reg_script_run_on_dev(dev, no_dev_behind_port);
 		dev->enabled = 0;

commit 16e758cf10c0db52b82a5e01694aabcc9a8e108a

Baytrail: fix build breakage
Fix build breakage caused by dd53d7f2de50a772c9a1ca43cab7d8431806bf2d.

BUG=None
TEST=compiled
BRANCH=NONE
Signed-off-by: Ted Kuo <tedkuo@ami.com.tw>

Change-Id: I98036cee0b6b519f2a8a21eed64a44f23fabb7c3
Reviewed-on: https://chromium-review.googlesource.com/217257
Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>

src/soc/intel/baytrail/pcie.c
@@ -163,7 +163,7 @@ static void check_device_present(device_t dev)
 	/* All PCIe ports disappear if port1 is disabled.
 	 * Keep PCIe port1 enabled even if no PCIe device present. */
 	if (!(pci_read_config32(dev, SLCTL_SLSTS) & PDS) &&
-			(is_first_port() == false)) {
+			(is_first_port(dev) == 0)) {
 		/* No device present. */
 		printk(BIOS_DEBUG, "No PCIe device present.\n");
 		reg_script_run_on_dev(dev, no_dev_behind_port);