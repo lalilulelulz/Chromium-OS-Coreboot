f272309b4f929b2bc0ee842ac7740433a8d3f9f9
David Hendricks <dhendrix@chromium.org>
2014-11-06 15:09:27 -0800

    gpio: cosmetic changes to tristate_gpios.c
    This patch makes a few cosmetic changes:
    - Rename tristate_gpios.c to gpio.c since it will soon be used for
      binary GPIOs as well.
    - Rename gpio_get_tristates() to gpio_base3_value() - The binary
      version will be called gpio_base2_value().
    - Updates call sites.
    - Change the variable name "id" to something more generic.
    
    When cherry-picking copied the below files to avoid conflicts:
    	src/mainboard/google/nyan_big/boardid.c
    	src/mainboard/google/nyan_blaze/boardid.c
    	src/mainboard/google/rush_ryu/boardid.c
    
    BUG=none
    BRANCH=none
    TEST=compiled for veyron_pinky and storm
    
    Change-Id: I36d88c67cb118efd1730278691dc3e4ecb6055ee
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/228324
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/237762

--- src/mainboard/google/nyan_big/boardid.c
+++ src/mainboard/google/nyan_big/boardid.c
@@ -17,22 +17,21 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <console/console.h>
-#include <soc/nvidia/tegra124/gpio.h>
-#include <stdlib.h>
-
 #include <boardid.h>
+#include <console/console.h>
+#include <gpio.h>
+#include <stdlib.h>
 
 uint8_t board_id(void)
 {
 	static int id = -1;
+	gpio_t gpio[] = {[3] = GPIO(X4), [2] = GPIO(X1),	/* X4 is MSB */
+			 [1] = GPIO(T1), [0] = GPIO(Q3),};	/* Q3 is LSB */
 
 	if (id < 0) {
-		gpio_t gpio[] = {GPIO(Q3), GPIO(T1), GPIO(X1), GPIO(X4)};
+		id = gpio_base3_value(gpio, ARRAY_SIZE(gpio));
 
-		id = gpio_get_in_tristate_values(gpio, ARRAY_SIZE(gpio), 0);
-
-		printk(BIOS_SPEW, "Board TRISTATE ID: %#x.\n", id);
+		printk(BIOS_SPEW, "Board TRISTATE ID: %d.\n", id);
 	}
 
 	return id;
