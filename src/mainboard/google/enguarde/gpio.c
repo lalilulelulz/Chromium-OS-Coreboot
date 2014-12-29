be74f552098d739e9a1e4b281b0495f18248217e
Keith Tzeng <keith.tzeng@quantatw.com>
2014-12-29 18:30:33 +0800

    Enguarde: Hoofer, Enable 20K internal pull up for GPIO S503
    BUG=chrome-os-partner:34962
    TEST= Build and confirm issue fixed.
    Signed-off-by: Keith Tzeng <keith.tzeng@quantatw.com>
    
    Change-Id: I65f6f1329d638de40835abb121189a6606ee7a24
    Reviewed-on: https://chromium-review.googlesource.com/237734
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- src/mainboard/google/enguarde/gpio.c
+++ src/mainboard/google/enguarde/gpio.c
@@ -166,7 +166,7 @@ static const struct soc_gpio_map gpssus_gpio_map[] = {
 	GPIO_ACPI_WAKE,	/* S500 - PCH_WAKE# */
 	GPIO_ACPI_WAKE,	/* S501 - TRACKPAD_INT# - INT */
 	GPIO_ACPI_WAKE,	/* S502 - TOUCH_INT# - INT */
-	GPIO_FUNC6,	/* S503 - LTE_WAKE_L# - INT */
+	GPIO_FUNC(6, PULL_UP, 20K),	/* S503 - LTE_WAKE_L# - INT */
 	GPIO_NC,	/* S504 - SOC_JTAG2_TDO (NC/PU) */
 	GPIO_FUNC1,	/* S505 - SUS_CLK_WLAN (NC) */
 	GPIO_INPUT_PU,	/* S506 - PCH_SPI_WP */
