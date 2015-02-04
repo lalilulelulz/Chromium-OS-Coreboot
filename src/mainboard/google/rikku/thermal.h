3d38ad692e598d6519262bac004b1b929ab07cb4
david <david_wu@quantatw.com>
2015-02-04 17:15:10 +0800

    Rikku: update fan policy
    BUG=none
    BRANCH=rikku
    TEST=manual, can compile BIOS firmware
    
    Change-Id: I18da11d6335af0870ad5f6b3118b26c7562ff075
    Signed-off-by: David Wu <David_Wu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/246014
    Tested-by: David Wu <david_wu@quantatw.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Reviewed-by: David Wu <david_wu@quantatw.com>
    Commit-Queue: David Wu <david_wu@quantatw.com>

--- src/mainboard/google/rikku/thermal.h
+++ src/mainboard/google/rikku/thermal.h
@@ -22,13 +22,38 @@
 
 #define TEMPERATURE_SENSOR_ID		0	/* PECI */
 
+/* Fan is OFF */
+#define FAN4_THRESHOLD_OFF	0
+#define FAN4_THRESHOLD_ON	0
+#define FAN4_PWM		0x00
+
+/* Fan is at LOW speed */
+#define FAN3_THRESHOLD_OFF	35
+#define FAN3_THRESHOLD_ON	40
+#define FAN3_PWM		0x88
+
+/* Fan is at MEDIUM speed */
+#define FAN2_THRESHOLD_OFF	44
+#define FAN2_THRESHOLD_ON	48
+#define FAN2_PWM		0x94
+
+/* Fan is at HIGH speed */
+#define FAN1_THRESHOLD_OFF	53
+#define FAN1_THRESHOLD_ON	58
+#define FAN1_PWM		0xb5
+
+/* Fan is at FULL speed */
+#define FAN0_THRESHOLD_OFF	65
+#define FAN0_THRESHOLD_ON	70
+#define FAN0_PWM		0xc4
+
 /* Temperature which OS will shutdown at */
-#define CRITICAL_TEMPERATURE		104
+#define CRITICAL_TEMPERATURE	98
 
 /* Temperature which OS will throttle CPU */
-#define PASSIVE_TEMPERATURE		95
+#define PASSIVE_TEMPERATURE	95
 
 /* Tj_max value for calculating PECI CPU temperature */
-#define MAX_TEMPERATURE			105
+#define MAX_TEMPERATURE		105
 
 #endif
