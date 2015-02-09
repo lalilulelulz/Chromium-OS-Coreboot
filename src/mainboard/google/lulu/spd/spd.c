6efd8b781402584988da02f762995c5ee34d5f4f
Marc Jones <marc.jones@se-eng.com>
2015-02-09 15:45:54 -0700

    lulu: Fix 2GB memory configuration
    The second channel logic was missing a parenthesis causing
    2GB SKU to fail memory training.
    
    BUG=chrome-os-partner:35359
    TEST=Build and boot 2GB (one channel) configuration
    BRANCH=auron
    
    Change-Id: Id61c57c82ad47cc08da2745f495a5296e9e05d32
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    Reviewed-on: https://chromium-review.googlesource.com/247692
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- src/mainboard/google/lulu/spd/spd.c
+++ src/mainboard/google/lulu/spd/spd.c
@@ -131,8 +131,8 @@ void mainboard_fill_spd_data(struct pei_data *pei_data)
 		(spd_index * SPD_LEN), SPD_LEN);
 
 	/* CH1 not used in 2GB configurations */
-	if (!(spd_index == 0b0000) || (spd_index == 0b0011) ||
-	     (spd_index == 0b1010)) {
+	if (!((spd_index == 0b0000) || (spd_index == 0b0011) ||
+	      (spd_index == 0b1010))) {
 		memcpy(pei_data->spd_data[1][0],
 			((char *)CBFS_SUBHEADER(spd_file)) +
 			(spd_index * SPD_LEN), SPD_LEN);
