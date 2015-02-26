8ef803e1dc2c72b4fab5bfb41acec13824fd827c
david <david_wu@quantatw.com>
2015-02-26 21:29:19 +0800

    Guado: Set thermal fan table
    fan on/off | PWM
    -----------+------
    0C/0C      | 0
    50C/40C    | 32%
    63C/56C    | 53%
    70C/65C    | 73%
    95C/90C    | 100%
    
    BUG=chrome-os-partner:36037
    BRANCH=guado
    TEST=mosys sensor print all
    
    Change-Id: I61027a15d25ea0e7a87d9aa905c2fff212d70118
    Signed-off-by: David Wu <David_Wu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/254042
    Tested-by: David Wu <david_wu@quantatw.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: David Wu <david_wu@quantatw.com>

--- src/mainboard/google/guado/thermal.h
+++ src/mainboard/google/guado/thermal.h
@@ -28,27 +28,27 @@
 #define FAN4_PWM		0x00
 
 /* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	50
-#define FAN3_THRESHOLD_ON	55
-#define FAN3_PWM		0x76
+#define FAN3_THRESHOLD_OFF	40
+#define FAN3_THRESHOLD_ON	50
+#define FAN3_PWM		0x52
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	59
-#define FAN2_THRESHOLD_ON	65
-#define FAN2_PWM		0x98
+#define FAN2_THRESHOLD_OFF	56
+#define FAN2_THRESHOLD_ON	63
+#define FAN2_PWM		0x88
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	68
-#define FAN1_THRESHOLD_ON	75
-#define FAN1_PWM		0xbf
+#define FAN1_THRESHOLD_OFF	65
+#define FAN1_THRESHOLD_ON	70
+#define FAN1_PWM		0xbb
 
 /* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	80
-#define FAN0_THRESHOLD_ON	86
-#define FAN0_PWM		0xdc
+#define FAN0_THRESHOLD_OFF	90
+#define FAN0_THRESHOLD_ON	95
+#define FAN0_PWM		0xff
 
 /* Temperature which OS will shutdown at */
-#define CRITICAL_TEMPERATURE	98
+#define CRITICAL_TEMPERATURE	104
 
 /* Temperature which OS will throttle CPU */
 #define PASSIVE_TEMPERATURE	95
