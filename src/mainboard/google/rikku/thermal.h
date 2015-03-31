6ed197847226ed2f82edae8129596bce373ef62d
ren kuo <ren.kuo@quantatw.com>
2015-03-31 14:48:33 +0800

    Rikku: Update thermal table
    fan on/off | PWM
    -----------+------
    0C/0C      | 52%
    47C/42C    | 65%
    59C/54C    | 70%
    71C/66C    | 79%
    83C/78C    | 85%
    
    BUG=none
    BRANCH=rikku
    TEST=mosys sensor print all
    
    Change-Id: I1519769f170c1e4921da7ba4c9fab5b2d934d0e1
    Signed-off-by: Ren Kuo <ren.kuo@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/263168
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Shawn N <shawnn@chromium.org>
    Tested-by: Shawn N <shawnn@chromium.org>
    (cherry picked from commit 64e353c9456e09d56de4cb72de73f43d52e4a1d0)
    Reviewed-on: https://chromium-review.googlesource.com/263243

--- src/mainboard/google/rikku/thermal.h
+++ src/mainboard/google/rikku/thermal.h
@@ -25,30 +25,30 @@
 /* Fan is OFF */
 #define FAN4_THRESHOLD_OFF	0
 #define FAN4_THRESHOLD_ON	0
-#define FAN4_PWM		0x00
+#define FAN4_PWM		0x84
 
 /* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	35
-#define FAN3_THRESHOLD_ON	40
-#define FAN3_PWM		0x88
+#define FAN3_THRESHOLD_OFF	42
+#define FAN3_THRESHOLD_ON	47
+#define FAN3_PWM		0xa5
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	44
-#define FAN2_THRESHOLD_ON	48
-#define FAN2_PWM		0x94
+#define FAN2_THRESHOLD_OFF	54
+#define FAN2_THRESHOLD_ON	59
+#define FAN2_PWM		0xb2
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	53
-#define FAN1_THRESHOLD_ON	58
-#define FAN1_PWM		0xb5
+#define FAN1_THRESHOLD_OFF	66
+#define FAN1_THRESHOLD_ON	71
+#define FAN1_PWM		0xc9
 
 /* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	65
-#define FAN0_THRESHOLD_ON	70
-#define FAN0_PWM		0xc4
+#define FAN0_THRESHOLD_OFF	78
+#define FAN0_THRESHOLD_ON	83
+#define FAN0_PWM		0xd8
 
 /* Temperature which OS will shutdown at */
-#define CRITICAL_TEMPERATURE	98
+#define CRITICAL_TEMPERATURE	100
 
 /* Temperature which OS will throttle CPU */
 #define PASSIVE_TEMPERATURE	95
