mtk09422/chromiumos-third_party-coreboot

broadwell: Skip pre-graphics delay in resume path
If the board is configured with a pre-graphics delay it should
be skipped in the resume path.

BUG=chrome-os-partner:28234
BRANCH=broadwell
TEST=measure resume time in dev mode to be same as normal mode

Change-Id: Ic9f2cda71d8a567f57e863409f0f3fb98ab68bcf
Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/245116
Reviewed-by: Shawn N <shawnn@chromium.org>

src/soc/intel/broadwell/igd.c
@@ -29,6 +29,7 @@
 #include <reg_script.h>
 #include <drivers/intel/gma/i915_reg.h>
 #include <soc/cpu.h>
+#include <soc/pm.h>
 #include <soc/ramstage.h>
 #include <soc/systemagent.h>
 #include <soc/intel/broadwell/chip.h>
@@ -487,13 +488,15 @@ static void igd_init(struct device *dev)
 		return;
 
 	/* Wait for any configured pre-graphics delay */
+	if (acpi_slp_type != SLEEP_STATE_S3) {
 #if IS_ENABLED(CONFIG_CHROMEOS)
-	if (developer_mode_enabled() || recovery_mode_enabled() ||
-	    vboot_wants_oprom())
-		mdelay(CONFIG_PRE_GRAPHICS_DELAY);
+		if (developer_mode_enabled() || recovery_mode_enabled() ||
+		    vboot_wants_oprom())
+			mdelay(CONFIG_PRE_GRAPHICS_DELAY);
 #else
-	mdelay(CONFIG_PRE_GRAPHICS_DELAY);
+		mdelay(CONFIG_PRE_GRAPHICS_DELAY);
 #endif
+	}
 
 	/* Early init steps */
 	if (is_broadwell) {

lalilulelulz/Chromium-OS-Coreboot

Reviewed-on: https://chromium-review.googlesource.com/262077
Commit-Queue: Shawn N <shawnn@chromium.org>
Tested-by: Shawn N <shawnn@chromium.org>
(cherry picked from commit e94399cf7c89e1526d815f3e8c870e0b4964b988)
Reviewed-on: https://chromium-review.googlesource.com/263371
Reviewed-by: Mike M Hsieh <mike.m.hsieh@intel.com>
Commit-Queue: Mike M Hsieh <mike.m.hsieh@intel.com>
Tested-by: Mike M Hsieh <mike.m.hsieh@intel.com>
(cherry picked from commit 678c221e42156f9f799b1a6122847ca736b375f8)
Reviewed-on: https://chromium-review.googlesource.com/263471

src/soc/intel/broadwell/igd.c
@@ -29,6 +29,7 @@
 #include <reg_script.h>
 #include <drivers/intel/gma/i915_reg.h>
 #include <broadwell/cpu.h>
+#include <broadwell/pm.h>
 #include <broadwell/ramstage.h>
 #include <broadwell/systemagent.h>
 #include <chip.h>
@@ -487,13 +488,15 @@ static void igd_init(struct device *dev)
 		return;
 
 	/* Wait for any configured pre-graphics delay */
+	if (acpi_slp_type != SLEEP_STATE_S3) {
 #if IS_ENABLED(CONFIG_CHROMEOS)
-	if (developer_mode_enabled() || recovery_mode_enabled() ||
-	    vboot_wants_oprom())
-		mdelay(CONFIG_PRE_GRAPHICS_DELAY);
+		if (developer_mode_enabled() || recovery_mode_enabled() ||
+		    vboot_wants_oprom())
+			mdelay(CONFIG_PRE_GRAPHICS_DELAY);
 #else
-	mdelay(CONFIG_PRE_GRAPHICS_DELAY);
+		mdelay(CONFIG_PRE_GRAPHICS_DELAY);
 #endif
+	}
 
 	/* Early init steps */
 	if (is_broadwell) {