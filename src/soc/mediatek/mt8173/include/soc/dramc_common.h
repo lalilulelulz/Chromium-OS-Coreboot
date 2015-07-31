9cd4ae4c392543e696eb4cac476ec744cdf910c3
Peter Kao <peter.kao@mediatek.com>
2015-07-31 17:11:14 +0800

    Oak: Add EMI driver and initialize DRAM
    BUG=none
    TEST=emerge-oak coreboot
    BRANCH=none
    
    Change-Id: I0f7b0a426dae1548b34114a024c92befdf6002f6
    Signed-off-by: Peter Kao <peter.kao@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/292692
    Commit-Ready: Yidi Lin <yidi.lin@mediatek.com>
    Tested-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/327894
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>
    Commit-Queue: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>

---
+++ src/soc/mediatek/mt8173/include/soc/dramc_common.h
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2015 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _DRAMC_COMMON_H_
+#define _DRAMC_COMMON_H_
+
+enum {
+	CHANNEL_A = 0,
+	CHANNEL_B,
+	CHANNEL_NUM
+};
+
+enum {
+	GW_PARAM_COARSE = 0,
+	GW_PARAM_FINE,
+	GW_PARAM_NUM
+};
+
+enum {
+	DUAL_RANKS = 2,
+	CATRAINING_NUM = 10
+};
+
+enum {
+	DQ_DATA_WIDTH = 32,
+	DQS_BIT_NUMBER = 8,
+	DQS_NUMBER = (DQ_DATA_WIDTH / DQS_BIT_NUMBER)
+};
+
+#endif   /* _DRAMC_COMMON_H_ */
