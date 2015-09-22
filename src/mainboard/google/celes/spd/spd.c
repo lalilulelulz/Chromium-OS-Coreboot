3b246becb4427f7b8ae54c09fa6e5da116102eec
Ted Kuo <tedkuo@ami.com.tw>
2015-09-22 17:19:06 +0800

    Celes: SMBIOS Table 17 - DIMM info
    Add support for the FSP_SMBIOS_MEMORY_INFO HOB which provides some of
    the information needed to populate the memory_info structure in CBMEM.
    The CBMEM data is later copied from CBMEM into SMBIOS table 17 by
    arch/x86/boot/smbios.c.
    Ported for Celes from https://chromium-review.googlesource.com/#/c/274105/
    patch of Braswell.
    
    BUG=chrome-os-partner:45084
    BRANCH=firmware-strago-7287.B
    TEST=Compiled, verified
    Signed-off-by: Ted Kuo <tedkuo@ami.com.tw>
    
    Change-Id: I9394d76a877aa821e9a54ca475355981591ffb24
    Reviewed-on: https://chromium-review.googlesource.com/300845
    Tested-by: Ted Kuo <tedkuo@ami.com.tw>
    Reviewed-by: shkim <sh_.kim@samsung.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Ted Kuo <tedkuo@ami.com.tw>

--- src/mainboard/google/celes/spd/spd.c
+++ src/mainboard/google/celes/spd/spd.c
@@ -19,9 +19,14 @@
  */
 
 #include <cbfs.h>
+#include <cbmem.h>
 #include <console/console.h>
-#include <lib.h>
 #include <gpio.h>
+#include <lib.h>
+#include <mainboard/google/celes/spd/spd.h>
+#include <memory_info.h>
+#include <smbios.h>
+#include <spd.h>
 #include <soc/gpio.h>
 #include <soc/romstage.h>
 #include <string.h>
@@ -104,11 +109,86 @@ void mainboard_fill_spd_data(struct pei_data *ps)
 	if (spd_content != NULL) {
 		ps->spd_data_ch0 = spd_content;
 		ps->spd_ch0_config = 1;
+		printk(BIOS_DEBUG, "Channel 0 DIMM soldered down\n");
 		if (dual_channel) {
+			printk(BIOS_DEBUG, "Channel 1 DIMM soldered down\n");
 			ps->spd_data_ch1 = spd_content;
 			ps->spd_ch1_config = 1;
 		} else {
+			printk(BIOS_DEBUG, "Channel 1 DIMM not installed\n");
 			ps->spd_ch1_config = 2;
 		}
+	}
+}
+
+static void set_dimm_info(uint8_t *spd, struct dimm_info *dimm)
+{
+	const int spd_capmb[8] = {  1,  2,  4,  8, 16, 32, 64,  0 };
+	const int spd_ranks[8] = {  1,  2,  3,  4, -1, -1, -1, -1 };
+	const int spd_devw[8]  = {  4,  8, 16, 32, -1, -1, -1, -1 };
+	const int spd_busw[8]  = {  8, 16, 32, 64, -1, -1, -1, -1 };
+	uint16_t clock_frequency;
+
+	int capmb = spd_capmb[spd[SPD_DENSITY_BANKS] & 7] * 256;
+	int ranks = spd_ranks[(spd[SPD_ORGANIZATION] >> 3) & 7];
+	int devw  = spd_devw[spd[SPD_ORGANIZATION] & 7];
+	int busw  = spd_busw[spd[SPD_BUS_DEV_WIDTH] & 7];
+
+	/* Parse the SPD data to determine the DIMM information */
+	dimm->ddr_type = MEMORY_DEVICE_LPDDR3;
+	dimm->dimm_size = capmb / 8 * busw / devw * ranks;  /* MiB */
+	clock_frequency = 1000 * spd[11] / (spd[10] * spd[12]);	/* MHz */
+	dimm->ddr_frequency = 2 * clock_frequency;	/* Double Data Rate */
+	dimm->mod_type = spd[3] & 0xf;
+	memcpy((char *)&dimm->module_part_number[0], &spd[0x80],
+		sizeof(dimm->module_part_number) - 1);
+	dimm->mod_id = *(uint16_t *)&spd[0x94];
+
+	switch (busw) {
+	default:
+	case 8:
+		dimm->bus_width = MEMORY_BUS_WIDTH_8;
+		break;
+
+	case 16:
+		dimm->bus_width = MEMORY_BUS_WIDTH_16;
+		break;
+
+	case 32:
+		dimm->bus_width = MEMORY_BUS_WIDTH_32;
+		break;
+
+	case 64:
+		dimm->bus_width = MEMORY_BUS_WIDTH_64;
+		break;
+	}
+}
+
+void mainboard_save_dimm_info(struct romstage_params *params)
+{
+	struct dimm_info *dimm;
+	struct memory_info *mem_info;
+
+	/*
+	 * Allocate CBMEM area for DIMM information used to populate SMBIOS
+	 * table 17
+	 */
+	mem_info = cbmem_add(CBMEM_ID_MEMINFO, sizeof(*mem_info));
+	printk(BIOS_DEBUG, "CBMEM entry for DIMM info: 0x%p\n", mem_info);
+	if (mem_info == NULL)
+		return;
+	memset(mem_info, 0, sizeof(*mem_info));
+
+	/* Describe the first channel memory */
+	dimm = &mem_info->dimm[0];
+	set_dimm_info(params->pei_data->spd_data_ch0, dimm);
+	mem_info->dimm_cnt = 1;
+
+	/* Describe the second channel memory */
+	if (params->pei_data->spd_ch1_config == 1) {
+		dimm = &mem_info->dimm[1];
+		set_dimm_info(params->pei_data->spd_data_ch1, dimm);
+		dimm->channel_num = 1;
+		mem_info->dimm_cnt = 2;
 	}
 }
