06a991296dd1cdb3b53c1369288de53e8d2a1a58
shkim <sh_.kim@samsung.com>
2015-09-30 09:32:49 +0900

    Celes: EC_IN_RW gpio input configuration
    Configuring EC_IN_RW signal as gpio input
    
    BUG=None
    BRANCH=firmware-strago-7287.B
    TEST=Test While in Recovery Mode use ctrl-d to switch to Dev Mode
    Signed-off-by: shkim <sh_.kim@samsung.com>
    
    Change-Id: Idcbd4e149b1e26a56637fdff3db03528a58b58e1
    Reviewed-on: https://chromium-review.googlesource.com/303100
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Commit-Queue: shkim <sh_.kim@samsung.com>
    Tested-by: shkim <sh_.kim@samsung.com>

--- src/mainboard/google/celes/gpio.c
+++ src/mainboard/google/celes/gpio.c
@@ -133,7 +133,7 @@ static const struct soc_gpio_map  gpsw_gpio_map[] = {
 	GPIO_INPUT_NO_PULL,/* 67  I2C3_SCL,RAMID1 */
 	GPIO_OUT_HIGH, /* 75 SATA_GP0 */
 	GPIO_NC, /* 76 GPI SATA_GP1 */
-	Native_M1, /* 77 SATA_LEDN */
+	GPIO_INPUT_PU_20K, /* 77 SATA_LEDN, EC_IN_RW */
 	GPIO_NC, /* 78 SATA_GP2 */
 	GPIO_NC, /* 79 MF_SMB_ALERTB */
 	GPIO_INPUT_NO_PULL, /* 80 SATA_GP3,RAMID0 */
