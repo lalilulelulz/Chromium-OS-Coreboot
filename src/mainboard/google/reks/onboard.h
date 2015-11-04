6124b7f5ab86ffdb6193cd5d46f1b65aaed88acb
Keith Tzeng <keith.tzeng@quantatw.com>
2015-11-04 15:45:51 +0800

    Reks: adjust I2C SDA hold time.
    I2C bus SDA hold time for touch devices is marginal with 60ns.
    Extend to 440ns.
    
    BUG=none
    BRANCH=firmware-strago-7287.B
    TEST=build and measure i2c sda hold time
    Signed-off-by: Keith Tzeng <Keith.Tzeng@quantatw.com>
    
    Change-Id: Ibb9449d439e2ca6b43f788e4be511d0483a029b8
    Reviewed-on: https://chromium-review.googlesource.com/310539
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- src/mainboard/google/reks/onboard.h
+++ src/mainboard/google/reks/onboard.h
@@ -69,4 +69,8 @@
 #define AUDIO_CODEC_I2C_ADDR		0x1A
 #define BCRD2_PMIC_I2C_BUS		0x01
 
+/* I2C data hold time */
+#define BOARD_I2C1_DATA_HOLD_TIME	0x1E
+#define BOARD_I2C6_DATA_HOLD_TIME	0x1E
+
 #endif
