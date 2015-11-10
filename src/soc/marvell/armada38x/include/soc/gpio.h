61d48e638cdb97dfe51d7728545e51c10e6cacb4
Ruilin Hao <rlhao@marvell.com>
2015-11-10 00:17:09 -0800

    soc/marvell/armada38x: Add gpio driver for armada38x
    Port gpio driver from uboot to coreboot
    
    BUG=chrome-os-partner:47462
    TEST=None
    BRANCH=tot
    
    Change-Id: Ia2e081a85347e2fc8bb365ca527ee2ee32af86f1
    Signed-off-by: Ruilin Hao <rlhao@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/313341
    Commit-Ready: Kan Yan <kyan@google.com>
    Tested-by: Kan Yan <kyan@google.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Kan Yan <kyan@google.com>
    Reviewed-by: Yuji Sasaki <sasakiy@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/327898
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>
    Commit-Queue: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>

---
+++ src/soc/marvell/armada38x/include/soc/gpio.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2015 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SOC_MARVELL_ARMADA38X_GPIO_H_
+#define __SOC_MARVELL_ARMADA38X_GPIO_H_
+
+#include <types.h>
+
+typedef u32 gpio_t;
+
+#endif  // __SOC_MARVELL_ARMADA38X_GPIO_H__
