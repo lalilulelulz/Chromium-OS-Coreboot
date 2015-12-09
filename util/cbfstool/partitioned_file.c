ef6a8e25d9e257d7de4cc6b94e510234fe20a56d
Vadim Bendebury <vbendeb@chromium.org>
2015-12-09 09:39:31 -0800

    cbfstool: accept read-only files when possible
    cbfstool tries opening the input file for write access even if the
    command does not require modifying the file.
    
    Let's not request write access unless it is necessary, this way one
    can examine write protected files without sudo.
    
    BRANCH=none
    BUG=none
    TEST=running
    
       cbfstool /build/<board>/firmware/image.bin print
    
       in chroot does not require root access any more.
    
    Change-Id: I871f32f0662221ffbdb13bf0482cb285ec184d07
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/317300
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- util/cbfstool/partitioned_file.c
+++ util/cbfstool/partitioned_file.c
@@ -53,11 +53,13 @@ static unsigned count_selected_fmap_entries(const struct fmap *fmap,
 	return count;
 }
 
-static partitioned_file_t *reopen_flat_file(const char *filename)
+static partitioned_file_t *reopen_flat_file(const char *filename,
+					    bool write_access)
 {
 	assert(filename);
-
 	struct partitioned_file *file = calloc(1, sizeof(*file));
+	const char *access_mode;
+
 	if (!file) {
 		ERROR("Failed to allocate partitioned file structure\n");
 		return NULL;
@@ -68,7 +70,9 @@ static partitioned_file_t *reopen_flat_file(const char *filename)
 		return NULL;
 	}
 
-	file->stream = fopen(filename, "rb+");
+	access_mode = write_access ?  "rb+" : "rb";
+	file->stream = fopen(filename, access_mode);
+
 	if (!file->stream) {
 		perror(filename);
 		partitioned_file_close(file);
@@ -165,11 +169,12 @@ partitioned_file_t *partitioned_file_create(const char *filename,
 	return file;
 }
 
-partitioned_file_t *partitioned_file_reopen(const char *filename)
+partitioned_file_t *partitioned_file_reopen(const char *filename,
+					    bool write_access)
 {
 	assert(filename);
 
-	partitioned_file_t *file = reopen_flat_file(filename);
+	partitioned_file_t *file = reopen_flat_file(filename, write_access);
 	if (!file)
 		return NULL;
 
