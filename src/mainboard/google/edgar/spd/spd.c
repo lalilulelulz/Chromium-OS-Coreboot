aad7b21e6752f88eed42ec02e2705bde410698e8
george_hou <george_hou@pegatroncorp.com>
2015-12-28 15:19:36 +0800

    Edgar: Add SPD data file per new keypart list
    Update SPD data file
    
    BUG=none
    BRANCH=firmware-strago-7287.B
    TEST=Build and boot pass
    Signed-off-by: george_hou <george_hou@pegatroncorp.com>
    
    Change-Id: Ib93078cea815a0bc2c97af80b297423b394aa1ad
    Reviewed-on: https://chromium-review.googlesource.com/319971
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Reviewed-by: george hou <George_Hou@pegatroncorp.com>
    Commit-Queue: george hou <George_Hou@pegatroncorp.com>
    Tested-by: george hou <George_Hou@pegatroncorp.com>

--- src/mainboard/google/edgar/spd/spd.c
+++ src/mainboard/google/edgar/spd/spd.c
@@ -36,8 +36,11 @@
  *   0b0001 - 4GiB total - 2 x 2GiB Hynix H9CCNNN8JTBLAR-NUD
  *   0b0010 - 2GiB total - 1 x 2GiB Samsung K4E8E304EE-EGCE
  *   0b0011 - 2GiB total - 1 x 2GiB Hynix H9CCNNN8JTBLAR-NUD
+ *   0b0100 - 4GiB total - 2 x 2GiB Samsung K4E8E324EB-EGCF
+ *   0b0101 - TBD
+ *   0b0110 - 2GiB total - 1 x 2GiB Samsung K4E8E324EB-EGCF
  */
-static const uint32_t dual_channel_config = (1 << 0) | (1 << 1);
+static const uint32_t dual_channel_config = (1 << 0) | (1 << 1) | (1 << 4);
 
 static void *get_spd_pointer(char *spd_file_content, int total_spds, int *dual)
 {
@@ -72,6 +75,15 @@ static void *get_spd_pointer(char *spd_file_content, int total_spds, int *dual)
 		break;
 	case 3:
 		printk(BIOS_DEBUG, "2GiB Hynix H9CCNNN8JTBLAR-NUD\n");
+		break;
+	case 4:
+		printk(BIOS_DEBUG, "4GiB Samsung K4E8E324EB-EGCF\n");
+		break;
+	case 5:
+		printk(BIOS_DEBUG, "empty\n");
+		break;
+	case 6:
+		printk(BIOS_DEBUG, "2GiB Samsung K4E8E324EB-EGCF\n");
 		break;
 	}
 
