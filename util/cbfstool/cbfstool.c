a0b70819003447a776452f4b7abc514121e865ee
Aaron Durbin <adurbin@chromium.org>
2016-01-26 15:35:34 -0600

    UPSTREAM: util/cbfstool: add machine parseable print
    In order to more easily process the output of 'cbfstool print'
    with other tools provide a -k option which spits out the
    tab-separated header and fields:
    
    Name Offset Type Metadata Size Data Size Total Size
    
    ALIGN_UP(Offset + Total Size, 64) would be the start
    of the next entry. Also, one can analzye the overhead
    and offsets of each file more easily.
    
    Example output (note: tabs aren't in here):
    
    $ ./coreboot-builds/sharedutils/cbfstool/cbfstool test.serial.bin print
    -r FW_MAIN_A  -k
    Performing operation on 'FW_MAIN_A' region...
    Name	Offset	Type	Metadata Size	Data Size	Total Size
    cmos_layout.bin	0x0	cmos_layout	0x38	0x48c	0x4c4
    dmic-2ch-48khz-16b.bin	0x500	raw	0x48	0xb68	0xbb0
    dmic-2ch-48khz-32b.bin	0x10c0	raw	0x48	0xb68	0xbb0
    nau88l25-2ch-48khz-24b.bin	0x1c80	raw	0x48	0x54	0x9c
    ssm4567-render-2ch-48khz-24b.bin	0x1d40	raw	0x58	0x54	0xac
    ssm4567-capture-4ch-48khz-32b.bin	0x1e00	raw	0x58	0x54	0xac
    vbt.bin	0x1ec0	optionrom	0x38	0x1000	0x1038
    spd.bin	0x2f00	spd	0x38	0x600	0x638
    config	0x3540	raw	0x38	0x1ab7	0x1aef
    revision	0x5040	raw	0x38	0x25e	0x296
    font.bin	0x5300	raw	0x38	0x77f	0x7b7
    vbgfx.bin	0x5ac0	raw	0x38	0x32f8	0x3330
    locales	0x8e00	raw	0x28	0x2	0x2a
    locale_en.bin	0x8e40	raw	0x38	0x29f6	0x2a2e
    u-boot.dtb	0xb880	mrc_cache	0x38	0xff1	0x1029
    (empty)	0xc8c0	null	0x64	0xadf4	0xae58
    fallback/ramstage	0x17740	stage	0x38	0x15238	0x15270
    (empty)	0x2c9c0	null	0x64	0xd2c4	0xd328
    fallback/payload	0x39d00	payload	0x38	0x12245	0x1227d
    cpu_microcode_blob.bin	0x4bf80	microcode	0x60	0x17000	0x17060
    (empty)	0x63000	null	0x28	0x37cf98	0x37cfc0
    
    Original-Reviewed-on: https://review.coreboot.org/13475
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    
    BUG=chrome-os-partner:49764
    BUG=chromium:445938
    BRANCH=glados
    TEST=Built chell chromeos-bootimage, built, and booted.
    
    Change-Id: I1c5f8c1b5f2f980033d6c954c9840299c6268431
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/324094
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- util/cbfstool/cbfstool.c
+++ util/cbfstool/cbfstool.c
@@ -77,6 +77,7 @@ static struct param {
 	bool fill_partial_downward;
 	bool show_immutable;
 	bool stage_xip;
+	bool machine_parseable;
 	int fit_empty_entries;
 	enum comp_algo compression;
 	enum vb2_hash_algorithm hash;
@@ -784,8 +785,10 @@ static int cbfs_print(void)
 	if (cbfs_image_from_buffer(&image, param.image_region,
 							param.headeroffset))
 		return 1;
-	cbfs_print_directory(&image);
-	return 0;
+	if (param.machine_parseable)
+		return cbfs_print_parseable_directory(&image);
+	else
+		return cbfs_print_directory(&image);
 }
 
 /* Forward declared so there aren't type collisions with cbfstool proper
@@ -1016,7 +1019,7 @@ static const struct command commands[] = {
 	{"hashcbfs", "r:R:A:vh?", cbfs_hash, true, true},
 	{"extract", "H:r:n:f:vh?", cbfs_extract, true, false},
 	{"layout", "wvh?", cbfs_layout, false, false},
-	{"print", "H:r:vh?", cbfs_print, true, false},
+	{"print", "H:r:vkh?", cbfs_print, true, false},
 	{"read", "r:f:vh?", cbfs_read, true, false},
 	{"remove", "H:r:n:vh?", cbfs_remove, true, true},
 	{"update-fit", "H:r:n:x:vh?", cbfs_update_fit, true, true},
@@ -1054,6 +1057,7 @@ static struct option long_options[] = {
 	{"verbose",       no_argument,       0, 'v' },
 	{"with-readonly", no_argument,       0, 'w' },
 	{"xip",           no_argument,       0, 'y' },
+	{"mach-parseable",no_argument,       0, 'k' },
 	{NULL,            0,                 0,  0  }
 };
 
@@ -1352,6 +1356,9 @@ int main(int argc, char **argv)
 				break;
 			case 'y':
 				param.stage_xip = true;
+				break;
+			case 'k':
+				param.machine_parseable = true;
 				break;
 			case 'h':
 			case '?':
