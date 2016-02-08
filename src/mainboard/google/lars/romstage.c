a60ac10934348f1bfdc6dc38ff87fca898ffa77e
Jenny TC <jenny.tc@intel.com>
2016-02-08 14:44:30 +0530

    Lars: Turn on keyboard backlight in romstage
    Use the keyboard backlight to provide indication that the system is
    booting.  This is useful for determining that a system is in S0 and
    is running BIOS code.
    
    BUG=None
    BRANCH=none
    TEST=boot on Lars and see keyboard backlight come on early
    
    Change-Id: I4fede6cff85f4487cedfbccf6cc24c6380d905e0
    Signed-off-by: Jenny TC <jenny.tc@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/326531
    Commit-Queue: Divagar Mohandass <divagar.mohandass@intel.com>
    Tested-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/mainboard/google/lars/romstage.c
+++ src/mainboard/google/lars/romstage.c
@@ -21,6 +21,7 @@
 
 #include <string.h>
 #include <gpio.h>
+#include <ec/google/chromeec/ec.h>
 #include <soc/pei_data.h>
 #include <soc/pei_wrapper.h>
 #include <soc/romstage.h>
@@ -36,6 +37,10 @@ void mainboard_romstage_entry(struct romstage_params *params)
 		GPIO_MEM_CONFIG_2,
 		GPIO_MEM_CONFIG_3,
 	};
+
+	/* Turn on keyboard backlight to indicate we are booting */
+	if (params->power_state->prev_sleep_state != SLEEP_STATE_S3)
+		google_chromeec_kbbacklight(25);
 
 	params->pei_data->mem_cfg_id = gpio_base2_value(spd_gpios,
 							ARRAY_SIZE(spd_gpios));
