089d93c712431d1b5923e844137c558994555e95
Duncan Laurie <dlaurie@chromium.org>
2016-03-07 13:21:56 -0800

    fsp1.1: Mark graphics init done after SiliconInit phase
    If the VBT was provided to the FSP GOP driver then graphics init
    will be done as part of SiliconInit step and we can mark that
    when it is completed.
    
    This will result in the "oprom" flag being set properly in the
    coreboot gpio table and the netboot firmware will have video.
    
    BUG=chrome-os-partner:50864
    BRANCH=glados
    TEST=boot image.net.bin on chell and get working graphics
    without being setuck in a reboot loop thinking graphics needs
    to be started when it already has been.
    
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/331301
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    (cherry picked from commit eeb9d470d8118422feb39ca71106972f2882e240)
    Change-Id: Ic59bad27eb9f184ca3eba24643851bfadfe23ab5
    Reviewed-on: https://chromium-review.googlesource.com/331355

--- src/drivers/intel/fsp1_1/ramstage.c
+++ src/drivers/intel/fsp1_1/ramstage.c
@@ -14,6 +14,7 @@
  * GNU General Public License for more details.
  */
 
+#include <bootmode.h>
 #include <arch/acpi.h>
 #include <cbmem.h>
 #include <cbfs.h>
@@ -141,6 +142,11 @@ static void fsp_run_silicon_init(int is_s3_wakeup)
 	status = fsp_silicon_init(&silicon_init_params);
 	timestamp_add_now(TS_FSP_SILICON_INIT_END);
 	printk(BIOS_DEBUG, "FspSiliconInit returned 0x%08x\n", status);
+
+	/* Mark graphics init done after SiliconInit if VBT was provided */
+	if (IS_ENABLED(CONFIG_GOP_SUPPORT) &&
+	    silicon_init_params.GraphicsConfigPtr)
+		gfx_set_init_done(1);
 
 	display_hob_info(fsp_info_header);
 	soc_after_silicon_init();
