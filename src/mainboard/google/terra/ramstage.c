bbc1fc132eb211de3c82d236cfc736f3119b90f7
Luke_Yin@asus.com <Luke_Yin@asus.com>
2016-05-20 14:59:24 +0800

    Terra: Modify USB2 register settings for Terra 11"/13".
    Improve increased USB2 eye height for BSW D-step.
    
    BUG=chrome-os-partner:52796
    TEST=Build/Flash firmware and measure USB2 eye diagram.
    BRANCH=firmware-terra-7287.154.B
    CQ-DEPEND=CL:321508
    
    Change-Id: I25379f9c8dcecfb36ae29db52571b8799af5b381
    Signed-off-by: Luke_Yin@asus.com <Luke_Yin@asus.com>
    Reviewed-on: https://chromium-review.googlesource.com/347311
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- src/mainboard/google/terra/ramstage.c
+++ src/mainboard/google/terra/ramstage.c
@@ -23,6 +23,56 @@
 
 void mainboard_silicon_init_params(SILICON_INIT_UPD *params)
 {
+	uint8_t	boardid = 0;
+	uint8_t	projectid = 0;
+
 	params->ChvSvidConfig = SVID_PMIC_CONFIG;
 	params->PMIC_I2CBus = PMIC_I2C_BUS;
+
+	boardid = board_id();
+	projectid = (boardid >> 3) & 0x01;
+
+	if (projectid == TERRA2_PROJECT_ID) {
+		params->Usb2Port0PerPortPeTxiSet = 7;
+		params->Usb2Port0PerPortTxiSet = 6;
+		params->Usb2Port0IUsbTxEmphasisEn = 3;
+		params->Usb2Port0PerPortTxPeHalf = 1;
+		params->Usb2Port1PerPortPeTxiSet = 7;
+		params->Usb2Port1PerPortTxiSet = 6;
+		params->Usb2Port1IUsbTxEmphasisEn = 3;
+		params->Usb2Port1PerPortTxPeHalf = 1;
+		params->Usb2Port2PerPortPeTxiSet = 7;
+		params->Usb2Port2PerPortTxiSet = 6;
+		params->Usb2Port2IUsbTxEmphasisEn = 3;
+		params->Usb2Port2PerPortTxPeHalf = 1;
+		params->Usb2Port3PerPortPeTxiSet = 7;
+		params->Usb2Port3PerPortTxiSet = 6;
+		params->Usb2Port3IUsbTxEmphasisEn = 3;
+		params->Usb2Port3PerPortTxPeHalf = 1;
+		params->Usb2Port4PerPortPeTxiSet = 7;
+		params->Usb2Port4PerPortTxiSet = 6;
+		params->Usb2Port4IUsbTxEmphasisEn = 3;
+		params->Usb2Port4PerPortTxPeHalf = 1;
+	} else {
+		params->Usb2Port0PerPortPeTxiSet = 7;
+		params->Usb2Port0PerPortTxiSet = 5;
+		params->Usb2Port0IUsbTxEmphasisEn = 2;
+		params->Usb2Port0PerPortTxPeHalf = 1;
+		params->Usb2Port1PerPortPeTxiSet = 7;
+		params->Usb2Port1PerPortTxiSet = 3;
+		params->Usb2Port1IUsbTxEmphasisEn = 2;
+		params->Usb2Port1PerPortTxPeHalf = 1;
+		params->Usb2Port2PerPortPeTxiSet = 7;
+		params->Usb2Port2PerPortTxiSet = 3;
+		params->Usb2Port2IUsbTxEmphasisEn = 2;
+		params->Usb2Port2PerPortTxPeHalf = 1;
+		params->Usb2Port3PerPortPeTxiSet = 7;
+		params->Usb2Port3PerPortTxiSet = 6;
+		params->Usb2Port3IUsbTxEmphasisEn = 3;
+		params->Usb2Port3PerPortTxPeHalf = 1;
+		params->Usb2Port4PerPortPeTxiSet = 7;
+		params->Usb2Port4PerPortTxiSet = 3;
+		params->Usb2Port4IUsbTxEmphasisEn = 2;
+		params->Usb2Port4PerPortTxPeHalf = 1;
+	}
 }
