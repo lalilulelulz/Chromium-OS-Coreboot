bb3ceadf574f074f2b6903045d99f31568ab79b8
Kevin Chiu <Kevin.Chiu@quantatw.com>
2016-06-23 12:47:07 +0800

    BSW: add USB2 phy setting override and soc stepping identify helper.
    1. create hook function to override USB2 phy setting from board level.
    2. add SOC helper to identify BSW SOC stepping.
    
    BRANCH=firmware-reks-7287.133.B
    BUG=none
    TEST=Build and verified
    Signed-off-by: Kevin Chiu <Kevin.Chiu@quantatw.com>
    
    Change-Id: I7f5f8c240de9f78d3f2ea9864bf2791501979427
    Reviewed-on: https://chromium-review.googlesource.com/355360
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- src/soc/intel/braswell/chip.c
+++ src/soc/intel/braswell/chip.c
@@ -85,6 +85,10 @@ static void enable_dev(device_t dev)
 	}
 }
 
+__attribute__((weak)) void board_silicon_USB2_override(SILICON_INIT_UPD *params)
+{
+}
+
 void soc_silicon_init_params(SILICON_INIT_UPD *params)
 {
 	device_t dev = dev_find_slot(0, PCI_DEVFN(LPC_DEV, LPC_FUNC));
@@ -165,6 +169,8 @@ void soc_silicon_init_params(SILICON_INIT_UPD *params)
 	params->ISPEnable = config->ISPEnable;
 	params->ISPPciDevConfig = config->ISPPciDevConfig;
 	params->PcdSdDetectChk = config->PcdSdDetectChk;
+
+	board_silicon_USB2_override(params);
 }
 
 void soc_display_silicon_init_params(const SILICON_INIT_UPD *old,
@@ -358,3 +364,83 @@ static void pci_set_subsystem(device_t dev, unsigned vendor, unsigned device)
 struct pci_operations soc_pci_ops = {
 	.set_subsystem = &pci_set_subsystem,
 };
+
+/**
+  Return SoC stepping type
+
+  @retval SOC_STEPPING            SoC stepping type
+**/
+int SocStepping(void)
+{
+	device_t dev = dev_find_slot(0, PCI_DEVFN(LPC_DEV, LPC_FUNC));
+	u8 revid = pci_read_config8(dev, 0x8);
+
+	switch (revid & B_PCH_LPC_RID_STEPPING_MASK) {
+	case V_PCH_LPC_RID_A0:
+	  return SocA0;
+	case V_PCH_LPC_RID_A1:
+	  return SocA1;
+	case V_PCH_LPC_RID_A2:
+	  return SocA2;
+	case V_PCH_LPC_RID_A3:
+	  return SocA3;
+	case V_PCH_LPC_RID_A4:
+	  return SocA4;
+	case V_PCH_LPC_RID_A5:
+	  return SocA5;
+	case V_PCH_LPC_RID_A6:
+	  return SocA6;
+	case V_PCH_LPC_RID_A7:
+	  return SocA7;
+	case V_PCH_LPC_RID_B0:
+	  return SocB0;
+	case V_PCH_LPC_RID_B1:
+	  return SocB1;
+	case V_PCH_LPC_RID_B2:
+	  return SocB2;
+	case V_PCH_LPC_RID_B3:
+	  return SocB3;
+	case V_PCH_LPC_RID_B4:
+	  return SocB4;
+	case V_PCH_LPC_RID_B5:
+	  return SocB5;
+	case V_PCH_LPC_RID_B6:
+	  return SocB6;
+	case V_PCH_LPC_RID_B7:
+	  return SocB7;
+	case V_PCH_LPC_RID_C0:
+	  return SocC0;
+	case V_PCH_LPC_RID_C1:
+	  return SocC1;
+	case V_PCH_LPC_RID_C2:
+	  return SocC2;
+	case V_PCH_LPC_RID_C3:
+	  return SocC3;
+	case V_PCH_LPC_RID_C4:
+	  return SocC4;
+	case V_PCH_LPC_RID_C5:
+	  return SocC5;
+	case V_PCH_LPC_RID_C6:
+	  return SocC6;
+	case V_PCH_LPC_RID_C7:
+	  return SocC7;
+	case V_PCH_LPC_RID_D0:
+	  return SocD0;
+	case V_PCH_LPC_RID_D1:
+	  return SocD1;
+	case V_PCH_LPC_RID_D2:
+	  return SocD2;
+	case V_PCH_LPC_RID_D3:
+	  return SocD3;
+	case V_PCH_LPC_RID_D4:
+	  return SocD4;
+	case V_PCH_LPC_RID_D5:
+	  return SocD5;
+	case V_PCH_LPC_RID_D6:
+	  return SocD6;
+	case V_PCH_LPC_RID_D7:
+	  return SocD7;
+	default:
+	  return SocSteppingMax;
+	}
+}
