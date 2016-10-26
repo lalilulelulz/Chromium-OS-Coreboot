952adb743af736bb81d0c9b3fc10b45866567466
T.H.Lin <T.H_Lin@quantatw.com>
2016-10-26 16:51:35 +0800

    Gnawty/Olay: Add 2nd source memory(Micro)
    Gnawty/Olay board use new 2nd source memory(Micro)
    Add prevent floating pin.
    
    BUG=chrome-os-partner:59031
    BRANCH=Gnawty/Olay
    TEST=emerge-gnawty chromeos-coreboot-gnawty chromeos-bootimage
    depthcharge libpayload
    
    Change-Id: I19283ac9f3c09a468c958864bd1a23d04e1cb881
    Signed-off-by: T.H. Lin <T.H_Lin@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/403709
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- src/mainboard/google/gnawty/romstage.c
+++ src/mainboard/google/gnawty/romstage.c
@@ -27,14 +27,15 @@
 
 /*
  * RAM_ID[2:0] are on GPIO_SSUS[39:37]
- * 0b000 - 4GiB total - 2 x 2GiB Samsung K4B4G1646Q-HYK0 1600MHz
- * 0b001 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63AFR-PBA 1600MHz
- * 0b010 - 2GiB total - 2 x 1GiB Micron MT41K128M16JT-125:K 1600MHz
- * 0b011 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63MFR-PBA 1600MHz
- * 0b100 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63CFR-PBA 1600MHz
- * 0b101 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63AFR-PBA 1600MHz
- * 0b110 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63CFR-PBA 1600MHz
- * 0b111 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63MFR-PBA 1600MHz
+ * 0b0000 - 4GiB total - 2 x 2GiB Samsung K4B4G1646Q-HYK0 1600MHz
+ * 0b0001 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63AFR-PBA 1600MHz
+ * 0b0010 - 2GiB total - 2 x 1GiB Micron MT41K128M16JT-125:K 1600MHz
+ * 0b0011 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63MFR-PBA 1600MHz
+ * 0b0100 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63CFR-PBA 1600MHz
+ * 0b0101 - 2GiB total - 1 x 2GiB Hynix  H5TC4G63AFR-PBA 1600MHz
+ * 0b0110 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63CFR-PBA 1600MHz
+ * 0b0111 - 4GiB total - 2 x 2GiB Hynix  H5TC4G63MFR-PBA 1600MHz
+ * 0b1000 - 2GiB total - 1 x 2Gib Micro  MT41K256M16TW-107
 */
 static const uint32_t dual_channel_config =
 	(1 << 0) | (1 << 1) | (1 << 2) | (1 << 6) | (1 << 7) ;
@@ -43,6 +44,7 @@ static const uint32_t dual_channel_config =
 #define GPIO_SSUS_37_PAD 57
 #define GPIO_SSUS_38_PAD 50
 #define GPIO_SSUS_39_PAD 58
+#define GPIO_SSUS_40_PAD 52
 
 static void *get_spd_pointer(char *spd_file_content, int total_spds, int *dual)
 {
@@ -54,10 +56,12 @@ static void *get_spd_pointer(char *spd_file_content, int total_spds, int *dual)
 	ssus_disable_internal_pull(GPIO_SSUS_37_PAD);
 	ssus_disable_internal_pull(GPIO_SSUS_38_PAD);
 	ssus_disable_internal_pull(GPIO_SSUS_39_PAD);
+	ssus_enable_internal_pull(GPIO_SSUS_40_PAD, PAD_PULL_DOWN | PAD_PU_20K);
 
 	ram_id |= (ssus_get_gpio(GPIO_SSUS_37_PAD) << 0);
 	ram_id |= (ssus_get_gpio(GPIO_SSUS_38_PAD) << 1);
 	ram_id |= (ssus_get_gpio(GPIO_SSUS_39_PAD) << 2);
+	ram_id |= (ssus_get_gpio(GPIO_SSUS_40_PAD) << 3);
 
 	printk(BIOS_DEBUG, "ram_id=%d, total_spds: %d\n", ram_id, total_spds);
 
