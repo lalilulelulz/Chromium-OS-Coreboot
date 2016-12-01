1fcf92f6affb5bec992e731c70644ba742861e66
Duncan Laurie <dlaurie@chromium.org>
2016-12-01 11:39:05 -0800

    chell: Set new PL1 minimum value if VPD key is true
    If there is a VPD entry indicating a new D Cover is present in the
    chassis then set the PL1 minimum to 3.6W.
    
    BUG=chrome-os-partner:54718
    BRANCH=glados
    TEST=manual:
    1) boot with key not set in VPD, or key set to False, then verify
    the power limit 1 minimum is 3W:
    cat /sys/devices/pci0000:00/0000:00:04.0/power_limits/power_limit_0_min_uw
    3000000
    2) run "vpd -s new_d_cover=true && reboot"
    3) verify that power limit 1 minimum is 3.6W:
    cat /sys/devices/pci0000:00/0000:00:04.0/power_limits/power_limit_0_min_uw
    3600000
    
    Change-Id: Id990156a8a9e46436b3cd4185e784ed3c877cdb0
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/415552
    Commit-Queue: Duncan Laurie <dlaurie@google.com>
    Tested-by: Duncan Laurie <dlaurie@google.com>
    Trybot-Ready: Duncan Laurie <dlaurie@google.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/chell/acpi_tables.c
+++ src/mainboard/google/chell/acpi_tables.c
@@ -17,3 +17,23 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc.
  */
+
+#include <arch/acpi.h>
+#include <console/console.h>
+#include <soc/acpi.h>
+#include <soc/nvs.h>
+#include <string.h>
+#include <vendorcode/google/chromeos/cros_vpd.h>
+
+void acpi_mainboard_gnvs(global_nvs_t *gnvs)
+{
+	const char *vpd_key = "new_d_cover";
+	char vpd_val[8];
+
+	/* If VPD indicates new D-panel set appropriate PL1 minimum */
+	if (cros_vpd_gets(vpd_key, vpd_val, ARRAY_SIZE(vpd_val)) &&
+	    !strncmp(vpd_val, "true", 4)) {
+		printk(BIOS_INFO, "New D-Cover found, set PL1 min to 3.6W\n");
+		gnvs->pl1l = 3600;
+	}
+}
