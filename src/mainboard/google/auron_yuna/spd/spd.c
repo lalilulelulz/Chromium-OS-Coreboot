de211f3244683151d6ce330c1c02d86f655f8e21
Tim Chen <Tim-Chen@quantatw.com>
2017-01-09 10:36:33 +0800

    auron_yuna: Update RAMID table for 2nd source memory.
    remove unused memory spd Micron_4KTF25664HZ
    
    add 2nd source Micron and Samsung memory
    1.Samsung K4B4G1646E-BYK0
    2.Micron MT41K256M16TW-107
    
    BUG=chrome-os-partner:61541
    TEST=emerge-auron_yuna coreboot
    
    Change-Id: I7f510b9b63de2c457db27cc11b77fd315be33572
    Signed-off-by: Tim Chen <Tim-Chen@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/425472
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- src/mainboard/google/auron_yuna/spd/spd.c
+++ src/mainboard/google/auron_yuna/spd/spd.c
@@ -123,8 +123,8 @@ void mainboard_fill_spd_data(struct pei_data *pei_data)
 	memcpy(pei_data->spd_data[0][0],
 		((char *)CBFS_SUBHEADER(spd_file)) +
 		(spd_index * SPD_LEN), SPD_LEN);
-	/* Index 0-2 are 4GB config with both CH0 and CH1.
-	 * Index 4-6 are 2GB config with CH0 only. */
+	/* Index 0-3 are 4GB config with both CH0 and CH1.
+	 * Index 4-7 are 2GB config with CH0 only. */
 	if (spd_index > 3)
 		pei_data->dimm_channel1_disabled = 3;
 	else
