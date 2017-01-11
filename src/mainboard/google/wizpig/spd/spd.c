8e3136e5ea59ad7fab9d072a1d56f667f526dc51
Peggy Chuang <PeggyChuang@ami.com.tw>
2017-01-11 11:59:50 +0800

    Wizpig: Add memory SPD info for Hynix H9CCNNN8JTBLAR-NUD
    Add current available ram_id to support Hynix H9CCNNN8JTBLAR-NUD spd info.
    Use RAM_ID3 for indicate dual channel configs.
    RAM_ID: 0100 2GB Hynix H9CCNNN8JTBLAR-NUD.
    RAM_ID: 1100 4GB Hynix H9CCNNN8JTBLAR-NUD.
    
    BUG=chrome-os-partner:61721
    TEST=Compiled
    BRANCH=firmware-strago-7287.B
    Signed-off-by: PeggyChuang <peggychuang@ami.com.tw>
    
    Change-Id: I5a1f901e5f906cf1929b43dd930a17b19b4a44bf
    Reviewed-on: https://chromium-review.googlesource.com/426442
    Reviewed-by: Charles Yuan <yuanch@haier.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Peggy Chuang <PeggyChuang@ami.com.tw>
    Tested-by: Peggy Chuang <PeggyChuang@ami.com.tw>

--- src/mainboard/google/wizpig/spd/spd.c
+++ src/mainboard/google/wizpig/spd/spd.c
@@ -37,11 +37,13 @@
  * 0b0001 - 2GiB total - 1 x 2GiB Hynix H9CCNNN8GTMLAR-NUD 1600MHz
  * 0b0010 - 2GiB total - 1 x 2GiB Micron EDF8132A3MA-JD-F 1600MHz
  * 0b0011 - 2GiB total - 1 x 2GiB Samsung K4E8E324EB-EGCF 1600MHz
+ * 0b0100 - 2GiB total - 1 x 2GiB Hynix H9CCNNN8JTBLAR-NUD 1866MHz
  *
  * 0b1000 - 4GiB total - 2 x 2GiB Samsung K4E8E304EE-EGCE 1600MHz
  * 0b1001 - 4GiB total - 2 x 2GiB Hynix H9CCNNN8GTMLAR-NUD 1600MHz
  * 0b1010 - 4GiB total - 2 x 2GiB Micron EDF8132A3MA-JD-F 1600MHz
  * 0b1011 - 4GiB total - 2 x 2GiB Samsung K4E8E324EB-EGCF 1600MHz
+ * 0b1100 - 4GiB total - 2 x 2GiB Hynix H9CCNNN8JTBLAR-NUD 1866MHz
  *
  */
 
