e8dd2d85405c3ebcf23999d047345228ed90afc8
Jeffy Chen <jeffy.chen@rock-chips.com>
2017-03-03 18:24:02 +0800

    bob: tpm: cr50: add irq clear/irq status for tpm irq
    BUG=b:35647967
    TEST=boot from bob
    
    Change-Id: I50c053ab7a6f6c14daee4fb2ab1cdcaeee2d67da
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/452286
    Commit-Ready: Caesar Wang <wxt@rock-chips.com>
    Tested-by: Caesar Wang <wxt@rock-chips.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/451052
    Commit-Queue: Caesar Wang <wxt@rock-chips.com>

--- src/mainboard/google/gru/chromeos.c
+++ src/mainboard/google/gru/chromeos.c
@@ -19,6 +19,7 @@
 #include <ec/google/chromeec/ec_commands.h>
 #include <gpio.h>
 #include <vendorcode/google/chromeos/chromeos.h>
+#include <tpm.h>
 
 #include "board.h"
 
@@ -37,6 +38,9 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 		{GPIO_EC_IN_RW.raw, ACTIVE_HIGH, -1, "EC in RW"},
 		{GPIO_EC_IRQ.raw, ACTIVE_LOW, -1, "EC interrupt"},
 		{GPIO_RESET.raw, ACTIVE_HIGH, -1, "reset"},
+#if IS_ENABLED(CONFIG_GRU_HAS_TPM2)
+		{GPIO_TPM_IRQ.raw, ACTIVE_HIGH, -1, "TPM interrupt"},
+#endif
 	};
 
 	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
@@ -62,3 +66,10 @@ void setup_chromeos_gpios(void)
 	gpio_input_pullup(GPIO_EC_IN_RW);
 	gpio_input_pullup(GPIO_EC_IRQ);
 }
+
+#if IS_ENABLED(CONFIG_GRU_HAS_TPM2)
+int tis_plat_irq_status(void)
+{
+	return gpio_irq_status(GPIO_TPM_IRQ);
+}
+#endif
