ffb9fc5eb7c2ac59eed700ec05230e83a2756ebb
Dmitry Torokhov <dtor@chromium.org>
2017-11-28 12:30:07 -0800

    Celes: use GpioInt for Keyboard IRQ
    Remove the hard coded IRQ number for the keyboard interrupt, and
    instead describe it as GpioInt() resource.
    
    BUG=chromium:704198
    TEST=Boot Celes with 4.14, verify keyboard works.
    
    Change-Id: Icfe5c3995007164bf617575b541758c18ee63a1d
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/798061
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/celes/onboard.h
+++ src/mainboard/google/celes/onboard.h
@@ -34,17 +34,12 @@
  * GPSE_SIZE = 86
  */
 
-/*
- * gpio based irq for kbd, 17th index in North Bank
- * MAX_DIRECT_IRQ + GPSW_SIZE + 18
- */
-/* ToDo: change kbd irq to gpio bank index */
-#define BOARD_I8042_IRQ			182
-
 /* Audio: Gpio index in SW bank */
 #define JACK_DETECT_GPIO_INDEX		95
 /* SCI: Gpio index in N bank */
 #define BOARD_SCI_GPIO_INDEX		15
+/* KBD: Gpio index in N bank */
+#define BOARD_I8042_GPIO_INDEX		17
 /* Trackpad: Gpio index in N bank */
 #define BOARD_TRACKPAD_GPIO_INDEX	18
 
